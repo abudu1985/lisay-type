57f0fe23a24108b92a5f66b13131c82efbce1bb4
igor.l <luchkoigor85@gmail.com>
2021-04-25 13:29:09 +0300

    done

--- src/facts.js
+++ src/facts.js
@@ -1,4 +1,4 @@
-export const facts = [`Three presidents, all Founding Fathers—John Adams, Thomas Jefferson, and James Monroe—died on July 4. Presidents Adams and Jefferson also died the same year, 1826; President Monroe died in 1831. Coincidence? You decide.`,
+export const facts = [`Three presidents, all Founding Fathers - John Adams, Thomas Jefferson, and James Monroe - died on July 4. Presidents Adams and Jefferson also died the same year, 1826; President Monroe died in 1831. Coincidence? You decide.`,
     `The Barbie doll's full name is Barbara Millicent Roberts, from Willows, Wisconsin. Her birthday is March 9, 1959, when she was first displayed at the New York Toy Fair.`,
     `There actually aren't "57 varieties" of Heinz ketchup, and never were. Company founder H.J. Heinz thought his product should have a number, and he liked 57. Hint: Hit the glass bottle on the "57," not the bottom, to get the ketchup to flow.`,
     `The tallest man ever recorded was American giant Robert Wadlow (1918-1940), who stood 8 feet 11 inches. Wadlow's size was the result of abnormally enlarged pituitary gland.`,
@@ -9,4 +9,7 @@ export const facts = [`Three presidents, all Founding Fathers—John Adams, Thom
     `The Four Corners is the only spot in the US where you can stand in four states at once: Utah, Colorado, Arizona and New Mexico.`,
     `The original name for the search engine Google was Backrub. It was renamed Google after the googol, which is the number one followed by 100 zeros.`,
     `The heart of the blue whale, the largest animal on earth, is five feet long and weighs 400 pounds. The whale in total weighs 40,000 pounds.`,
-    `For comparison, an elephant's heart weighs around 30 pounds. And a human heart? A mere 10 ounces.`];
+    `For comparison, an elephant's heart weighs around 30 pounds. And a human heart? A mere 10 ounces.`,
+    `Cows don't actually have four stomachs; they have one stomach with four compartments.`,
+    `Polar bears have black skin. And actually, their fur isn't white - it's see-through, so it appears white as it reflects light.`
+];
